41e73031afdd797fc9cc6ed87f67f856491593db
dell <dell@DESKTOP-I89L11M>
2024-02-08 22:17:35 +0530

    push push push code

---
+++ src/main/java/headset/boy.java
@@ -0,0 +1,10 @@
+package headset;
+
+public class boy {
+
+	public static void main(String[] args) {
+  System.out.println("push code from headset package");
+
+	}
+
+}
---
+++ src/test/java/classRoom/girl.java
@@ -0,0 +1,10 @@
+package classRoom;
+
+public class girl {
+
+	public static void main(String[] args) {
+		System.out.println("i am push code from classroom package");
+
+	}
+
+}
